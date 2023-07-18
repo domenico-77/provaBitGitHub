b9f494a728e1c5eea5963f898e41137a3f8474dc
Domenico Vicenti <79905785+domenico-77@users.noreply.github.com>
2023-07-18 12:37:59 +0200

    Update crea_csv.py

--- crea_csv.py
+++ crea_csv.py
@@ -4,8 +4,16 @@ import os
 # Definisci il percorso del file CSV da creare
 file_path = "../output/output.csv"
 
+# Estrai il percorso della directory dal percorso del file
+directory = os.path.dirname(file_path)
+
+# Verifica se la directory esiste, altrimenti crea la directory
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
 # Definisci i dati da scrivere nel file CSV
 data = [["ciao"]]
+
 if os.path.isfile(file_path):
     data = [["ciao2"]]
     with open(file_path, mode='a', newline='') as file:
@@ -16,5 +24,5 @@ else:
         writer = csv.writer(file)
         writer.writerows(data)
 
-
 print("Il file CSV è stato creato correttamente.")
+
